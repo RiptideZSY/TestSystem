ccd0c75dfc0e388f36f904264561f060bb2c2a6e
RiptideZSY <17816890390@163.com>
2017-12-22 10:02:06 +0800

    配置d3

--- client/src/d3charts/d3Component.js
+++ client/src/d3charts/d3Component.js
@@ -1,20 +1,25 @@
-import React from 'React'
+import React from 'react'
+import PropTypes from 'prop-types'
+import {
+  autorun
+} from 'mobx'
 //import d3 from 'd3'
 
-export default class D3Components extends React.Component {
+export default class D3Component extends React.Component {
+  static propTypes = {
+    width: PropTypes.number,
+    height: PropTypes.number,
+  }
+  componentDidMount() {
+    autorun(() => this.renderGraph(this.g, this.props))
+  }
+  renderGraph = () => {
+    console.log('请实现renderGraph方法');
+  }
 
-	constructor(props) {
-		super(props);
-		this.renderGraph = this.renderGraph.bind(this);
-	}
-
-	renderGraph = () => {
-   		console.log('请实现renderGraph方法')
-  	}
-
-  	render() {
-   		return (
-     		<g ref={(g) => { this.g = g }} width={this.props.width} height={this.props.height} />
-    	)
-  	}
+  render() {
+    return (
+      <g ref={(g) => { this.g = g }} width={this.props.width} height={this.props.height} />
+    )
+  }
 }
--- webpack.config.js
+++ webpack.config.js
@@ -11,64 +11,58 @@ module.exports = {
     contentBase: path.join(__dirname, "client/dist")
   },
   module: {
-    rules: [
-      {
-        test : /\.js[x]?$/,
-        exclude : /(node_modules|bower_components)/,
-        use: [ 
-          {
-            loader: "babel-loader"
-          }
-        ]
-      },
-      {
-        test: /\.css$/,
-        use: [
-          "style-loader",
-          "css-loader"
-        ]
-      },
-      {
-        test: /\.(png|svg|jpg|gif)$/,
-        use: [
-          "file-loader",
-          {
-            loader: "image-webpack-loader",
-            options: {
-              bypassOnDebug: true,
-            },
+    rules: [{
+      test: /\.js[x]?$/,
+      exclude: /(node_modules|bower_components)/,
+      use: [{
+        loader: "babel-loader"
+      }]
+    }, {
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        "css-loader"
+      ]
+    }, {
+      test: /\.(png|svg|jpg|gif)$/,
+      use: [
+        "file-loader", {
+          loader: "image-webpack-loader",
+          options: {
+            bypassOnDebug: true,
           },
-        ]
-      },
-      {
-        test: /\.(png|jpg|gif)$/,
-        use: [
-          {
-            loader: "url-loader",
-            options: {
-              limit: 8192
-            }
-          }
-        ]
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/,
-        use: [
-          "file-loader"
-        ]
-      },
-      {
-        test: /\.(csv|tsv)$/,
-        use: [
-          "csv-loader"
-        ]
-      },
-      {
-        test: /\.xml$/,
-        use: [
-          "xml-loader"
-        ]
+        },
+      ]
+    }, {
+      test: /\.(png|jpg|gif)$/,
+      use: [{
+        loader: "url-loader",
+        options: {
+          limit: 8192
+        }
+      }]
+    }, {
+      test: /\.(woff|woff2|eot|ttf|otf)$/,
+      use: [
+        "file-loader"
+      ]
+    }, {
+      test: /\.(csv|tsv)$/,
+      use: [
+        "csv-loader"
+      ]
+    }, {
+      test: /\.xml$/,
+      use: [
+        "xml-loader"
+      ]
+    }, {
+      test: /\.(jsx|js)$/,
+      loader: 'babel-loader',
+      exclude: /node_modules/,
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
       }
-    ]
+    }]
   }
 };

82a0e077d880528b9ca9e22a2b00fff42ab16e55
RiptideZSY <17816890390@163.com>
2018-01-02 10:53:38 +0800

    解决了独立出刷新按钮组件后不能实时刷新的bug

--- client/src/mainContent.js
+++ client/src/mainContent.js
@@ -1,6 +1,7 @@
 import React from "react";
 import BarChart from "./d3charts/barChart"
 import LineChart from "./d3charts/lineChart"
+import DropDown from "./dropDown"
 import {
 	observer
 } from 'mobx-react'
@@ -104,7 +105,18 @@ function ContentProgressBar(props) {
          			</div>
         		</div>
 			</div>*/
+function ScheduleBar(props) {
+	return (
+		<div id="scheduleBar" onMouseMove={props.onMouseMove} onMouseUp={props.onMouseUp}>        		
+	        <input type="text" value={props.inputValue} autoComplete="off" onChange={props.onChange}/>%
+	        <div id="backBar">
+	            <div id="circle" onMouseDown={props.onMouseDown} style={props.styleCircle}></div>
+	            <div id="frontBar" style={props.styleFrontBar}></div>
+	        </div>
+        </div>
+	);
 
+}
 class ContentScheduleBar extends React.Component {
 	constructor(props) {
 		super(props);
@@ -112,12 +124,12 @@ class ContentScheduleBar extends React.Component {
 		this.handleMouseMove = this.handleMouseMove.bind(this);
 		this.handleMouseUp = this.handleMouseUp.bind(this);
 		this.handleInputChange = this.handleInputChange.bind(this);
-		this.handleFlushData = this.handleFlushData.bind(this);
-		var datatmp = [];
-		var dataCount = 20;
-		for (var i = 0; i < dataCount; i++) {
-			datatmp.push(Math.round(Math.random() * 500));
-		}
+		//this.handleFlushData = this.handleFlushData.bind(this);
+		// var datatmp = [];
+		// var dataCount = 20;
+		// for (var i = 0; i < dataCount; i++) {
+		// 	datatmp.push(Math.round(Math.random() * 500));
+		// }
 		this.state = {
 			isfalse: false, //控制鼠标点击
 			inputValue: 0,
@@ -126,9 +138,10 @@ class ContentScheduleBar extends React.Component {
 			barWidth: 0, //进度条的总宽度
 			circleMarginLeft: 0, //圆离进度条最左边的距离
 			frontBarWidth: 0,
-			myData: datatmp
+			//myData: datatmp
 		};
 	}
+	//控制鼠标点击拖动
 	handleMouseDown(event) {
 		var currentX = event.clientX;
 		var circle = document.getElementById("circle");
@@ -166,6 +179,7 @@ class ContentScheduleBar extends React.Component {
 		});
 
 	}
+	//处理用户输入
 	handleInputChange(event) {
 		var minValue = 0;
 		var maxValue = 100;
@@ -188,16 +202,16 @@ class ContentScheduleBar extends React.Component {
 			});
 		}
 	}
-	handleFlushData() {
-		var datatmp = [];
-		var dataCount = 20;
-		for (var i = 0; i < dataCount; i++) {
-			datatmp.push(Math.round(Math.random() * 500));
-		}
-		this.setState({
-			myData: datatmp
-		});
-	}
+	// handleFlushData() {
+	// 	var datatmp = [];
+	// 	var dataCount = 20;
+	// 	for (var i = 0; i < dataCount; i++) {
+	// 		datatmp.push(Math.round(Math.random() * 500));
+	// 	}
+	// 	this.setState({
+	// 		myData: datatmp
+	// 	});
+	// }
 	render() {
 		var styleCircle = {
 			marginLeft: this.state.circleMarginLeft
@@ -205,19 +219,17 @@ class ContentScheduleBar extends React.Component {
 		var styleFrontBar = {
 			width: this.state.frontBarWidth
 		};
+		//var arr = ["宽度", "坐标轴", "整体"];
 		return (
 			<div>
-				<div id="scheduleBar" onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp}>
-	        		<input type="text" value={this.state.inputValue} autoComplete="off" onChange={this.handleInputChange}/>%
-	        		<div id="backBar">
-	            		<div id="circle" onMouseDown={this.handleMouseDown} style={styleCircle}></div>
-	            		<div id="frontBar" style={styleFrontBar}></div>
-	        		</div>
-        		</div>
-        		<button className="btn btn-success" onClick={this.handleFlushData}>刷新数据</button>
+				<ScheduleBar onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp}
+				onChange={this.handleInputChange} onMouseDown={this.handleMouseDown} styleCircle={styleCircle}
+				styleFrontBar = {styleFrontBar} inputValue={this.state.inputValue}/>
+				
+				
         		<svg id="main-svg" style={{width: 529.5, height: 529.5, top: 0, left: 0}}>
 			        <g>
-						<BarChart widthPercent = {this.state.inputValue} data= {this.state.myData}/>
+						<BarChart widthPercent = {this.state.inputValue} data= {this.props.data}/>
 			        </g>
 			    </svg>		       
 			</div>
@@ -225,19 +237,52 @@ class ContentScheduleBar extends React.Component {
 	}
 
 }
+/*<div id="scheduleBar" onMouseMove={this.handleMouseMove} onMouseUp={this.handleMouseUp}>
+	        		
+	        		<input type="text" value={this.state.inputValue} autoComplete="off" onChange={this.handleInputChange}/>%
+	        		<div id="backBar">
+	            		<div id="circle" onMouseDown={this.handleMouseDown} style={styleCircle}></div>
+	            		<div id="frontBar" style={styleFrontBar}></div>
+	        		</div>
+        		</div>*/
+class ContentBody extends React.Component {
 
-function ContentBody(props) {
-	return (
+	constructor(props) {
+		super(props);
+		this.handleFlushData = this.handleFlushData.bind(this);
+		var datatmp = [];
+		var dataCount = 20;
+		for (var i = 0; i < dataCount; i++) {
+			datatmp.push(Math.round(Math.random() * 500));
+		}
+		this.state = {
+			data: datatmp
+		};
+		//console.log(this.state.data);
+	}
+	handleFlushData() {
+		var datatmp = [];
+		var dataCount = 20;
+		for (var i = 0; i < dataCount; i++) {
+			datatmp.push(Math.round(Math.random() * 500));
+		}
+		this.setState({
+			data: datatmp
+		});
+		console.log(this.state.data);
+	}
 
-		<div className="row">
+	render() {
+		return (
+			<div className="row">
         		<div className="col-md-6">
         			<div className="box box-solid">
 	            		<div className="box-header with-border">
 	              			<h3 className="box-title">BarChart</h3>
 	              		</div>
-			            <div className="box-body">			            	
-							<ContentScheduleBar />
-			             		     
+			            <div className="box-body">	
+			           		<button className="btn btn-success" onClick={this.handleFlushData}>刷新数据</button>           	
+							<ContentScheduleBar data={this.state.data}/>		             		     
 			             </div>
 			        </div>
 			    </div>
@@ -247,17 +292,18 @@ function ContentBody(props) {
 	              			<h3 className="box-title">LineChart</h3>
 	              		</div>
 			            <div className="box-body">
-			            	
+							
 			            	<svg id="main-svg" style={{width: 529.5, height: 529.5, top: 0, left: 0}}>
-			        <g>
-						<LineChart />
-			        </g>
-			    </svg>	
+						        <g>
+									<LineChart />
+						        </g>
+						    </svg>	
 			            </div>
 			        </div>
 			    </div>
             </div>
-	);
+		);
+	}
 }
 //<ContentHeader />
 export default class MainContent extends React.Component {
